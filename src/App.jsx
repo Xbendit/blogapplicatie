3bff053f5dc48325fe187151db323495af77d944
Xbendit <vanrossumben@gmail.com>
2024-12-15 13:27:21 +0100

    finnally

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import './App.css'
-import logo from './assets/logo-white.png'
-/*import {BrowserRouter as Route} from "react-router-dom";*/
-import {Link, Routes, Route, useNavigate} from "react-router-dom"
+import {Routes, Route} from "react-router-dom"
 import Home from './pages/home/Home.jsx'
 import AllPosts from './pages/allPosts/AllPosts.jsx'
 import NewPosts from './pages/newPosts/NewPosts.jsx'
@@ -11,16 +9,9 @@ import BlogPost from "./pages/blogPost/BlogPost.jsx";
 
 
 function App() {
-    const navigate = useNavigate();
-
-    navigate ('/posts/');
 
     return (
-        /*<div className="page-container">
-
-            <img src={logo} alt="Company logo"/>
-            <h1>Begin hier met het maken van jouw blog-applicatie!</h1>*/
-           <>
+        <>
             <Navigation/>
             <Routes>
                 <Route path="/" element={<Home/>}/>
@@ -29,10 +20,7 @@ function App() {
                 <Route path="*" element={<NotFound/>}/>
                 <Route path="/posts/:postID" element={<BlogPost/>}/>
             </Routes>
-           </>
-       /* </div>*/
-
-
+        </>
     )
 }
 
--- src/pages/newPosts/NewPosts.jsx
+++ src/pages/newPosts/NewPosts.jsx
@@ -1,11 +1,90 @@
 import "./NewPosts.css"
+import {useForm} from 'react-hook-form'
+import {useNavigate} from "react-router-dom";
+import ReadTime from "../../assets/helpers/ReadTime.jsx";
 
-function NewPosts(){
 
-    return(
-        <>
-        <h1>Dit is de NewPost pagina</h1>
-        </>
+function NewPosts() {
+    const {register, handleSubmit} = useForm();
+    const navigate = useNavigate()
+    const todaydate = new Date();
+    const today = todaydate.toISOString()
+
+
+
+
+    function onSubmit(data) {
+        const readTime = ReadTime(data["message-content"]);
+        const postData = {
+            ...data,
+            readTime, // Voeg leestijd toe
+        };
+        console.log(postData)
+        navigate("/");
+    }
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <label htmlFor="title-field">
+                Titel:
+                <input
+                    type="text"
+                    id="name-field"
+                    {...register("title", {required: true, maxLength: 30})}
+                />
+            </label>
+
+            <label htmlFor="subtitle-field">
+                Subtitel:
+                <input
+                    type="text"
+                    id="subtitel-field"
+                    {...register("subtitel", {required: true, maxLength: 30})}
+                />
+            </label>
+
+            <label htmlFor="author-field">
+                Auteur:
+                <input
+                    type="text"
+                    id="author-field"
+                    {...register("author", {required: true, maxLength: 30})}
+                />
+            </label>
+
+            <label htmlFor="message-field">
+                Bericht:
+                <textarea
+                    id="message-field"
+                    rows="6"
+                    cols="80"
+                    placeholder="Maak een Post!"
+                    {...register("message-content", {required: true, maxLength: 2000, minLength: 300})}
+                >
+        </textarea>
+            </label>
+
+            <input
+                type="hidden"
+                value={today}
+                {...register("created")}
+            />
+            <input
+                type="hidden"
+                value={0}
+                {...register("comments")}
+            />
+            <input
+                type="hidden"
+                value={0}
+                {...register("shares")}
+            />
+
+            <button type="submit">
+                Versturen
+            </button>
+        </form>
     )
 }
+
 export default NewPosts;
